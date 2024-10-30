8282651fbc82316a939aeaf8f755635e533563e3
barnab2003 <barnabbarman@gmail.com>
2024-10-31 01:44:55 +0530

    All files added

---
+++ website/views.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/home', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST': 
+        note = request.form.get('note')
+        price = request.form.get('price')
+        number = request.form.get('number')
+        if len(note) < 1 and len(price) < 1 and len(number) :
+            flash('Note is too short!', category='error') 
+        else:
+            new_note = Note(data=note, price=price, number=number,  user_id=current_user.id)  # Include price here
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+@views.route('/homeforreal')
+@login_required
+def homeforreal():
+    return render_template("homeforreal.html", user= current_user)
+
+@views.route('/filter')
+@login_required
+def filter():
+    return render_template("filter.html", user= current_user)
+
+@views.route('/')
+@login_required
+def welcome():
+    return render_template("welcome.html", user= current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
